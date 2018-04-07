9f8f542dc1c08a1131215517bbf5c6976fd48ad6
postolenko <denis.postolenko@gmail.com>
2018-04-07 18:36:53 +0300

    Restourant Food

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,50 @@ e = d.documentElement,
 g = d.getElementsByTagName('body')[0],
 bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
 
+var shape;
+var shapeTempl;
+
 $(window).load(function() {
 
    
 
 });
 
-
 $(window).resize(function() {
 
-    getShapeParams()
+    bodyWidth = w.innerWidth || e.clientWidth || g.clientWidth;
+
+    getShapeParams();
+
+    getAnimation();
+
+    getImgPath();
+
+});
+
+$(document).scroll(function() {
+
+    getAnimation();
+
+    getImgPath();
 
 });
 
 $(document).ready(function() {
-   
-   $(".h-box").each(function() {
 
-        var shapeTempl = "<span class='shape'></span><span class='shape'></span>";
+    getAnimation();
 
-        $(this).prepend(shapeTempl);
+    getImgPath();
 
-   });
+    $(".h-box").each(function() {
 
-   getShapeParams();
+      shapeTempl = "<span class='shape'></span><span class='shape'></span>";
+
+      $(this).prepend(shapeTempl);
+
+    });
+
+    getShapeParams();
 
     $(".thumbnails-2").find(".thumb-2:even").addClass("even_th");
     $(".thumbnails-2").find(".even_th:odd").addClass("right_th");
@@ -38,12 +58,38 @@ function getShapeParams() {
 
     $(".h-box").each(function() {
 
-        var shape = $(this).find(".shape");
+        shape = $(this).find(".shape");
 
         shape.css({
             "width" : ( $(this).closest(".row").width() - $(this).find("h2").outerWidth(true) ) / 2 + "px"
         });
 
    });
+
+}
+
+function getAnimation() {
+
+  $("section").each(function() {
+
+    if( $(this).offset().top <= $(document).scrollTop() + $(window).height() ) {
+
+      $(this).addClass("active");
+
+    }
+
+  });  
+
+}
+
+function getImgPath() {
+
+    $("img").each(function() {
+        if($(this).offset().top <= $(document).scrollTop() + $(window).height()
+            && $(this).attr("src") == "" ) {
+            var imgPath = $(this).attr("data-src");
+            $(this).attr("src", imgPath);
+        }
+    });
 
 }
